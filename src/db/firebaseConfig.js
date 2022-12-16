44f9d68f01d9e1b91765103248dfc1202f9afebc
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2022-12-16 11:23:15 -0500

    Update firebaseConfig.js

--- src/db/firebaseConfig.js
+++ src/db/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig ={
-  apiKey: "AIzaSyB_1Z4cPgS6YfOxOFBiKhshsqBGaaJJHJ8",
-  authDomain: "whatsappclone-1e469.firebaseapp.com",
-  projectId: "whatsappclone-1e469",
-  storageBucket: "whatsappclone-1e469.appspot.com",
-  messagingSenderId: "999531167227",
-  appId: "1:999531167227:web:2c2010174c75033aad9f5e"
+  apiKey: "AIzaSyA74ZvfzfZVmopLVsbl88Y02CUaXNWGgwk",
+  authDomain: "roblox-29707.firebaseapp.com",
+  projectId: "roblox-29707",
+  storageBucket: "roblox-29707.appspot.com",
+  messagingSenderId: "1025034692104",
+  appId: "1:1025034692104:web:628690bd4a9e9bf83293bd
 };
 
 export default firebaseConfig;
