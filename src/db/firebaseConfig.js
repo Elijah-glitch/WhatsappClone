3aef2091d690eea9dae2261ecd3699e541ab5e6e
Znation31 <elijahenix@gmail.com>
2022-12-25 21:15:48 +0000

    hi

--- src/db/firebaseConfig.js
+++ src/db/firebaseConfig.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA--ei9WTUHVb52EvWNG3aZ2Pe_piEJXtQ",
-  authDomain: "tcapsmoviesv2.firebaseapp.com",
-  projectId: "tcapsmoviesv2",
-  storageBucket: "tcapsmoviesv2.appspot.com",
-  messagingSenderId: "842410031578",
-  appId: "1:842410031578:web:c5924f5c88b7e2cf56e8e3"
+const  firebaseConfig  = {
+  apiKey : "AIzaSyA--ei9WTUHVb52EvWNG3aZ2Pe_piEJXtQ" ,
+  authDomain : "tcapsmoviesv2.firebaseapp.com" ,
+  projectId : "tcapsmoviesv2" ,
+  storageBucket : "tcapsmoviesv2.appspot.com" ,
+  messagingSenderId : "842410031578" ,
+  appId : "1:842410031578:web:c5924f5c88b7e2cf56e8e3"
 };
 
-export default firebaseConfig;
+export  default  firebaseConfig ;
